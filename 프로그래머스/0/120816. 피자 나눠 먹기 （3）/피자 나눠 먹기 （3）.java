81a2edd5d4dce5fdd7fb93da52cbeb6b1bbb6f2a
하진 <khjtwin11@gmail.com>
2025-05-27 23:11:41 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        for(int i = 0; i<=answer; i++)
+        if (slice*answer<n)
+            answer++;
+        return answer;
+    }
+}
