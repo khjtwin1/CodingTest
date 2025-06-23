844a7e0f78d554b8fa5d6bf2842d7a7c67536f73
하진 <khjtwin11@gmail.com>
2025-06-24 00:00:30 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        if (num==1)
+            return 0;
+        
+        for (int i = 1; i <= 500; i++){
+            if(num % 2 == 0)
+                num /= 2;
+            else
+                num = num * 3 + 1;
+            if(num == 1)
+                return i;
+        }
+        return -1;
+    }
+}
