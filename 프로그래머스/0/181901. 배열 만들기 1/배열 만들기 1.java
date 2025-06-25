1ecbdea06e0f1e43ff642030035d9b81c8f6bd88
하진 <khjtwin11@gmail.com>
2025-06-25 11:02:17 +0900

    [level 0] Title: 배열 만들기 1, Time: 8.48 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int cnt = 0;
+        for (int i = 0; i<n; i++){
+            if((i+1)%k==0)
+                cnt++;
+        }
+        int[] answer = new int[cnt];
+        cnt= 0;
+        for (int i = 0; i<n; i++){
+            if((i+1)%k==0){
+                answer[cnt]=i+1;
+                cnt++;
+            }
+        }
+        return answer;
+    }
+}
