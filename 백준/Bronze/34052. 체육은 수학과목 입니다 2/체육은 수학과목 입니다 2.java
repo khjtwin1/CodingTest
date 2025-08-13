06c9bd1cf29193e4255da85e1883f68fa27838ca
하진 <khjtwin11@gmail.com>
2025-08-13 17:12:16 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.java
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        int t1 = sc.nextInt();
+        int t2 = sc.nextInt();
+        int t3 = sc.nextInt();
+        int t4 = sc.nextInt();
         
-        if(a+b+c+d+300<=1800){
+        int total = t1 + t2 + t3 + t4 + 300;
+        
+        if(total <= 1800){
             System.out.println("Yes");
         } else{
             System.out.println("No");
