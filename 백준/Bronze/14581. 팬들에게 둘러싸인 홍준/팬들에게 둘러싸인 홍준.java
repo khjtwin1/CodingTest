ee9e4e5ff16ff1adf4aa5115397378a20337566c
하진 <khjtwin11@gmail.com>
2025-08-19 00:01:36 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 188 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String ID = sc.next();
+        System.out.println(":fan::fan::fan:\n:fan::" + ID + "::fan:\n:fan::fan::fan:");
+    }
+}
