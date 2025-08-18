e7c13c8d163f5d9423a5807ac951b3556e47797c
하진 <khjtwin11@gmail.com>
2025-08-18 16:36:50 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i<5; i++){
+            int n = sc.nextInt();
+            sum += n*n;
+        }
+        int check = sum % 10;
+        System.out.println(check);
+    }
+}
