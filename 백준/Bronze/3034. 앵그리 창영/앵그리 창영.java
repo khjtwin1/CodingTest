540d33c317a86250e3d6b9585f4ff66f5f72185f
하진 <khjtwin11@gmail.com>
2025-08-25 18:53:07 +0900

    [Bronze III] Title: 앵그리 창영, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int W = sc.nextInt();
+        int H = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int L = sc.nextInt();
+            if (L * L <= W * W + H * H)
+                System.out.println("DA");
+            else
+                System.out.println("NE");
+        }
+    }
+}
