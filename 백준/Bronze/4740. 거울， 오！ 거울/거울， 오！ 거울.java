ccdb5428ffaf0841dcdd702f5d8e1eaf7129b42c
하진 <khjtwin11@gmail.com>
2025-09-03 22:48:23 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 168 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String str = sc.nextLine();
+            if(str.equals("***")){
+                return;
+            }
+            for(int i = str.length()-1; i>=0; i--){
+                System.out.print(str.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+}
