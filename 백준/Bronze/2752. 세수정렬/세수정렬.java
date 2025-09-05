9ea5f7c8a5dd027ec8942e868e6b4569d7a960c7
하진 <khjtwin11@gmail.com>
2025-09-05 13:15:35 +0900

    [Bronze IV] Title: 세수정렬, Time: 188 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = i + 1; j < 3; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
