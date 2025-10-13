61148b89d858e13532274cbb74b626916e42ba04
하진 <khjtwin11@gmail.com>
2025-10-14 00:25:39 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+        int arr = 0;
+        for(int i = 0; i<str_list.length; i++){
+            if(str_list[i].equals("l")){
+                answer = new String[i];
+                for(int j = 0; j<i; j++){
+                    answer[j] = str_list[j];
+                }
+                break;
+            }
+            if(str_list[i].equals("r")){
+                answer = new String[str_list.length-i-1];
+                int k = 0;
+                for(int j = i+1; j<str_list.length; j++){
+                    answer[k++] = str_list[j];
+                }
+                break;
+            }
+        }
+        if(arr == 0){
+            return answer;
+        }
+        return answer;
+    }
+}
