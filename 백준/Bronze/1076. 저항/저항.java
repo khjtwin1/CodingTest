e6d01eaf5af0bcf95285033b0e5030cbd6e3deff
하진 <khjtwin11@gmail.com>
2025-11-09 23:59:50 +0900

    [Bronze II] Title: 저항, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -1,38 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String resistor = "";
-        for(int i = 0; i<2; i++) {
-            String c = sc.next();
-            switch (c){
-                case "black" : resistor += "0"; break;
-                case "brown" : resistor += "1"; break;
-                case "red" : resistor += "2"; break;
-                case "orange" : resistor += "3"; break;
-                case "yellow" : resistor += "4"; break;
-                case "green" : resistor += "5"; break;
-                case "blue" : resistor += "6"; break;
-                case "violet" : resistor += "7"; break;
-                case "grey" : resistor += "8"; break;
-                default: resistor += "9";
-            }
-        }
-        long answer = Long.parseLong(resistor);
-        String c3 = sc.next();
-        switch (c3){
-            case "black" : answer*=1; break;
-            case "brown" : answer*=10; break;
-            case "red" : answer*=100; break;
-            case "orange" : answer*=1000; break;
-            case "yellow" : answer*=10000; break;
-            case "green" : answer*=100000; break;
-            case "blue" : answer*=1000000; break;
-            case "violet" : answer*=10000000; break;
-            case "grey" : answer*=100000000; break;
-            default: answer*=1000000000; break;
-        }
+        Map<String, Integer> map = new HashMap<>();
+        map.put("black",0);
+        map.put("brown",1);
+        map.put("red",2);
+        map.put("orange",3);
+        map.put("yellow",4);
+        map.put("green",5);
+        map.put("blue",6);
+        map.put("violet",7);
+        map.put("grey",8);
+        map.put("white",9);
+
+        int c1 = map.get(sc.next());
+        int c2 = map.get(sc.next());
+        int c3 = map.get(sc.next());
+        long answer = (c1 * 10 + c2) * (long)(Math.pow(10,c3));
         System.out.println(answer);
     }
 }
