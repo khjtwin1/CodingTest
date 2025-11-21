6c3a371f59fe9935cb0544e85e3145e62290d12d
하진 <khjtwin11@gmail.com>
2025-11-21 23:07:46 +0900

    [Bronze III] Title: 저작권, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int I = sc.nextInt();
+        int answer = A*(I-1)+1;
+        System.out.println(answer);
+    }
+}
