d433459ceadd007c56ebfd17c6f7c24d98bdbecd
하진 <khjtwin11@gmail.com>
2025-11-25 18:17:45 +0900

    [level 0] Title: 배열 만들기 4, Time: 19.68 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        for(int i = 0; i<arr.length; i+=0){
+            if(stk.isEmpty()){
+                stk.add(arr[i]);
+                i++;
+            } else if(stk.get(stk.size()-1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size()-1);
+            }
+        }
+        int[] answer = new int[stk.size()];
+        for(int i = 0; i<answer.length; i++){
+            answer[i] = stk.get(i);
+        }
+        return answer;
+    }
+}
