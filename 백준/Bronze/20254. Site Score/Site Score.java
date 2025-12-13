260033366c2cd0aa6ed30759deb7e45159854304
하진 <khjtwin11@gmail.com>
2025-12-13 23:34:33 +0900

    [Bronze V] Title: Site Score, Time: 164 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int UR = sc.nextInt();
+        int TR = sc.nextInt();
+        int UO = sc.nextInt();
+        int TO = sc.nextInt();
+        System.out.println(56*UR + 24*TR + 14*UO +6 *TO);
+    }
+}
