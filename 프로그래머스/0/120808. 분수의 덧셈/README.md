3ad37762761019fab6a52e3f03570a0491285fbd
하진 <khjtwin11@gmail.com>
2025-12-31 00:14:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 9.16 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808#) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 10.52 ms
+메모리: 88.6 MB, 시간: 9.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 31일 00:13:30
+2025년 12월 31일 00:14:30
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int s = (numer1 * denom2) + (numer2 * denom1);//9
-        int m = denom1 * denom2;// 18
-        int b = Math.max(s, m);//9
+        int s = (numer1 * denom2) + (numer2 * denom1);
+        int m = denom1 * denom2;
+        int b = Math.max(s, m);
         int n = 0;
         for (int i = 1; i <= b; i++) {
             if (s % i == 0 && m % i == 0) {
