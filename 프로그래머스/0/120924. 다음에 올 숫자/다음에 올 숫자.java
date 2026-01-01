e418935fea661b16812a440e3dba5aa0394cf99d
하진 <khjtwin11@gmail.com>
2026-01-01 23:14:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = common[1] - common[0] == common[2] - common[1] ? common[common.length - 1] + (common[1] - common[0]) : common[common.length - 1] * (common[1] / common[0]);
+        return answer;
+    }
+}
