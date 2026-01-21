f66700d9d00d70b0b76405c5e1686e6febdfb322
하진 <khjtwin11@gmail.com>
2026-01-21 22:59:43 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 164 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        switch (s) {
+            case "SONGDO":
+                System.out.println("HIGHSCHOOL");
+                break;
+            case "CODE":
+                System.out.println("MASTER");
+                break;
+            case "2023":
+                System.out.println("0611");
+                break;
+            default:
+                System.out.println("CONTEST");
+        }
+    }
+}
