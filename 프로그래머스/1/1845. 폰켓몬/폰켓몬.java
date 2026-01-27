da1edf13fd48456ec8aa05e7343b414bb0a2d8b4
하진 <khjtwin11@gmail.com>
2026-01-27 23:35:27 +0900

    [level 1] Title: 폰켓몬, Time: 6.80 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> list = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        for (int i = list.size(); i > 0; i--) {
+            if (i <= nums.length / 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
