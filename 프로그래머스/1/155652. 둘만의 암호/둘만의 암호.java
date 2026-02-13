b27bff91fa1310a7ac0a115b4448c0c52c7b8229
하진 <khjtwin11@gmail.com>
2026-02-14 02:53:55 +0900

    [level 1] Title: 둘만의 암호, Time: 14.08 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -4,15 +4,14 @@ class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         ArrayList<Character> skipArr = new ArrayList<>();
-        for (int i = 0; i < skip.length(); i++) {
-            skipArr.add(skip.charAt(i));
+        for (char c : skip.toCharArray()) {
+            skipArr.add(c);
         }
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (char c : s.toCharArray()) {
             int cnt = 0;
             while (cnt < index) {
-                if (c == 'z') c = 'a';
-                else c++;
+                c++;
+                if (c > 'z') c = 'a';
                 if (!skipArr.contains(c)) cnt++;
             }
             answer += c;
