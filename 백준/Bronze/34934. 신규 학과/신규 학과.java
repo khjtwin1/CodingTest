e95c1e17d698d2eb277f25d8c543319a5e1d4ecb
하진 <khjtwin11@gmail.com>
2026-03-07 23:34:06 +0900

    [Bronze IV] Title: 신규 학과, Time: 176 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/34934. 신규 학과/신규 학과.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            String name = sc.next();
+            int year = sc.nextInt();
+            if (year == 2026) {
+                System.out.print (name);
+            }
+        }
+    }
+}
